9613582827eb2de246c364674eb258c407032731
Sébastien PONT <seba12.04@orange.fr>
2022-12-19 02:34:49 +0100

    feat: :sparkles: add color syntax on code

---
+++ src/components/Code/Code.tsx
@@ -0,0 +1,50 @@
+import CodeEditor from '@uiw/react-textarea-code-editor';
+import React from "react";
+import { Compiler } from '../../utils';
+import { propsTypes } from "./Code.props";
+
+// import "./Code.css";
+
+const Code = (props: propsTypes) => {
+    const { textFile } = props;
+
+    // useEffect(() => {
+    //     document.documentElement.setAttribute('data-color-mode', 'dark')
+    // }, [])
+
+    const [code, setCode] = React.useState(textFile);
+
+    const runCode = () => {
+        const transpiledOutput = Compiler(code);
+        console.log(transpiledOutput.diagnostics);
+        if (transpiledOutput.diagnostics) {
+            if (transpiledOutput.diagnostics.length > 0) {
+                console.log("Error");
+            }
+            // else {
+            //     eval(transpiledOutput.outputText);
+            // }
+        }
+    }
+
+    return (
+        <div className="code">
+
+            <CodeEditor
+                value={code}
+                language="ts"
+                placeholder="Please enter  code."
+                padding={15}
+                style={{
+                    fontSize: 12,
+                    // backgroundColor: "transparent",
+                    fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+                }}
+                onChange={(evn) => setCode(evn.target.value)}
+            />
+            <button onClick={runCode}>Run</button>
+        </div>
+    )
+}
+
+export default Code;
