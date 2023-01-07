dc8d0bc88c0b0abfd494a87376e87db644a66da9
Sébastien PONT <seba12.04@orange.fr>
2023-01-07 23:05:22 +0100

    feat: :lipstick: add tooltip if var name is too long

--- src/components/FnParams/FnParams.utils.ts
+++ src/components/FnParams/FnParams.utils.ts
@@ -1,5 +1,14 @@
-const shorten = (name: string, MAX: number = 10): string => {
-    return (name.slice(0, MAX));
+type shorten = {
+    name: string,
+    shortened: boolean,
+}
+const shorten = (name: string, MAX: number = 10): shorten => {
+    if (name.length >= MAX) {
+        // keep the first 3 characters and the last 3 characters
+        const newName = `${name.slice(0, 3)}...${name.slice(-3)}`;
+        return { name: newName, shortened: true }
+    }
+    return ({ name, shortened: false });
 }
 
 export { shorten };
